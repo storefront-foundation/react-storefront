9167f88ca1411936fcc7cc80985ba0231c8da0f6
Richard van der Dys <richard.vanderdys@gmail.com>
2018-11-28 10:34:35 -0600

    Removed indexes from src
    with fixed conflicts

--- packages/react-storefront/src/stats/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright © 2017-2018 Moov Corporation.  All rights reserved.
- */
-// @create-index
-
-export { default as getStats } from './getStats.js';
-export { default as getStatsInDev } from './getStatsInDev.js';
-
