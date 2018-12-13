901974d5b5c222b9c7ab0312314f9f59b7189c22
Richard van der Dys <richard.vanderdys@gmail.com>
2018-12-12 16:53:02 -0800

    Updating storybook version

--- packages/react-storefront/.storybook/webpack.config.js
+++ packages/react-storefront/.storybook/webpack.config.js
@@ -3,6 +3,7 @@ module.exports = baseConfig => {
   	fetch: 'isomorphic-unfetch'
   }
   // Apply local show code plugin for example code generation
+  console.log(JSON.stringify(baseConfig.module, null, 3));
 	baseConfig.module.rules[0].query.plugins.push(require.resolve('./babel-plugin-show-code'));
   // Return the altered config
   return baseConfig
