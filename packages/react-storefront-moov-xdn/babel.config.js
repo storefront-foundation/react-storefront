6bd6f1c693b6629500cc6c20b4ae3199a0f0dc8f
Mark Brocato <mark.brocato@moovweb.com>
2019-02-15 06:42:22 -0800

    fixed docs build

--- packages/react-storefront-moov-xdn/babel.config.js
+++ packages/react-storefront-moov-xdn/babel.config.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "presets": ["@babel/env"],
   "plugins": [
     "@babel/plugin-proposal-class-properties"
--- packages/react-storefront/babel.config.js
+++ packages/react-storefront/babel.config.js
@@ -1,44 +1,48 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    "presets": [
-      ["@babel/env", {
-        "targets": {
-          "browsers": "> 1%"
-        },
-        "useBuiltIns": "usage",
-        "forceAllTransforms": true,
-        "modules": false
-      }],
-      "@babel/react"
-    ],
-    "env": {
-      "production": {
-        "presets": ["minify"]
+module.exports = {
+  "presets": [
+    ["@babel/env", {
+      "targets": {
+        "browsers": "> 1%"
       },
-      "test": {
-        "presets": [
-          ["@babel/env", {
-            "targets": {
-              "browsers": "> 1%"
-            },
-            "useBuiltIns": "usage",
-            "forceAllTransforms": true
-          }],
-          "@babel/react"
-        ]
-      }
+      "useBuiltIns": "usage",
+      "forceAllTransforms": true,
+      "modules": false
+    }],
+    "@babel/react"
+  ],
+  "env": {
+    "test": {
+      "presets": [
+        ["@babel/env", {
+          "targets": {
+            "browsers": "> 1%"
+          },
+          "useBuiltIns": "usage",
+          "forceAllTransforms": true
+        }],
+        "@babel/react"
+      ]
     },
-    "plugins": [
-      ["@babel/plugin-transform-runtime", {
-        "regenerator": true
-      }],
-      "@babel/plugin-transform-async-to-generator",
-      ["@babel/plugin-proposal-decorators", {
-        "legacy": true
-      }],
-      "@babel/plugin-proposal-object-rest-spread",
-      "@babel/plugin-proposal-class-properties"
-    ]
-  };
+    "docs": {
+      "presets": [
+        ["@babel/env", {
+          "targets": {
+            "node": "current"
+          }
+        }],
+        "@babel/react"
+      ]
+    }
+  },
+  "plugins": [
+    ["@babel/plugin-transform-runtime", {
+      "regenerator": true
+    }],
+    "@babel/plugin-transform-async-to-generator",
+    ["@babel/plugin-proposal-decorators", {
+      "legacy": true
+    }],
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-proposal-class-properties"
+  ]
 }
