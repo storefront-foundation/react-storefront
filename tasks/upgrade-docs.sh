7aa82cb8b85f3ca00ba79dfe1d7463fe17675849
Richard van der Dys <richard.vanderdys@gmail.com>
2019-02-21 19:54:59 +0200

    Adding git configs

--- tasks/upgrade-docs.sh
+++ tasks/upgrade-docs.sh
@@ -2,6 +2,9 @@ rm -Rf docs;
 git clone git@github.com:moovweb/moov-pwa-docs.git docs;
 node ./tasks/upgradeDocsVersion;
 cd docs;
+git config credential.helper 'cache --timeout=120'
+git config user.email "bot@circleci.com"
+git config user.name "Circle CI Bot"
 git add package.json;
 git commit -m "RSF Upgrade";
 git push;
