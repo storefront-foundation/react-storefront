114d7c9aad346b9ac0328bb67d4e731550a75e66
Mark Brocato <mark.brocato@moovweb.com>
2019-05-02 14:52:48 -0700

    updated indexes

--- packages/react-storefront/src/amp/index.js
+++ packages/react-storefront/src/amp/index.js
@@ -5,11 +5,12 @@
 
 // @create-index
 
-export { default as AmpExpandableSection } from './AmpExpandableSection.js'
-export { default as AmpForm } from './AmpForm.js'
-export { default as AmpHidden } from './AmpHidden.js'
-export { default as AmpImageSwitcher } from './AmpImageSwitcher.js'
-export { default as AmpMenu } from './AmpMenu.js'
-export { default as AmpModal } from './AmpModal.js'
-export { default as AmpState } from './AmpState.js'
-export { default as installServiceWorker } from './installServiceWorker.js'
+export { default as AmpExpandableSection } from './AmpExpandableSection.js';
+export { default as AmpForm } from './AmpForm.js';
+export { default as AmpHidden } from './AmpHidden.js';
+export { default as AmpImageSwitcher } from './AmpImageSwitcher.js';
+export { default as AmpMenu } from './AmpMenu.js';
+export { default as AmpModal } from './AmpModal.js';
+export { default as AmpState } from './AmpState.js';
+export { default as installServiceWorker } from './installServiceWorker.js';
+
--- packages/react-storefront/src/router/index.js
+++ packages/react-storefront/src/router/index.js
@@ -5,11 +5,12 @@
 
 // @create-index
 
-export { default as ClientContext } from './ClientContext.js'
-export { default as Router } from './Router.js'
-export { default as cache } from './cache.js'
-export { default as fromClient } from './fromClient.js'
-export { default as fromServer } from './fromServer.js'
-export { default as proxyUpstream } from './proxyUpstream.js'
-export { default as serviceWorker } from './serviceWorker.js'
-export { default as withGlobalState } from './withGlobalState.js'
+export { default as ClientContext } from './ClientContext.js';
+export { default as Router } from './Router.js';
+export { default as cache } from './cache.js';
+export { default as fromClient } from './fromClient.js';
+export { default as fromServer } from './fromServer.js';
+export { default as proxyUpstream } from './proxyUpstream.js';
+export { default as serviceWorker } from './serviceWorker.js';
+export { default as withGlobalState } from './withGlobalState.js';
+
