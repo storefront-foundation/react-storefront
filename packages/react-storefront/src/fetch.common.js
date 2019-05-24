409ef6fc56f276b55eac286bed0e8da156babacb
Matthew Tuusberg <tuusberg@gmail.com>
2019-05-24 12:36:41 +0300

    add fetch.common

---
+++ packages/react-storefront/src/fetch.common.js
@@ -0,0 +1,9 @@
+/**
+ * @license
+ * Copyright © 2017-2019 Moov Corporation.  All rights reserved.
+ */
+
+ /**
+  * Provides access to fetch method for modules that require() it.
+  */
+module.exports = require('./fetch').default
