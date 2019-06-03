8e83ceb5061ef05b867ab297d2284210790f6845
Richard van der Dys <richard.vanderdys@gmail.com>
2019-06-03 16:10:24 +0300

    Sane default cache key Pipe values together URL encode cache key

--- packages/react-storefront/src/edge/index.js
+++ packages/react-storefront/src/edge/index.js
@@ -3,14 +3,22 @@
  * Copyright © 2017-2019 Moov Corporation.  All rights reserved.
  */
 import { CLOUDFRONT_CACHE, XDN_VERSION } from '../router/headers'
+import querystring from 'querystring'
 
-export default function addCacheKey(router, event, context, callback) {
-  return buildCacheKey => {
+export default function addCacheKeys(router, event, context, callback) {
+  return buildCacheKeys => {
     const request = event.Records ? event.Records[0].cf.request : event
     const { match, params } = router.findMatchingRoute(request)
+    const version = __build_timestamp__ // eslint-disable-line
 
-    request.headers[CLOUDFRONT_CACHE] = buildCacheKey(match, params, request, context)
-    request.headers[XDN_VERSION] = __build_timestamp__ // eslint-disable-line
+    let cacheKey = buildCacheKeys(match, params, request, context)
+    if (!cacheKey || !cacheKey.length) {
+      // Might need a better default
+      cacheKey = [request.path, querystring.stringify(request.queryStringParameters || {}), version]
+    }
+
+    request.headers[CLOUDFRONT_CACHE] = encodeURIComponent(cacheKey.join('|'))
+    request.headers[XDN_VERSION] = version
 
     callback(null, request)
   }
