12a9bb3a8e0e7c1006464b5d91eee176b56a9a3d
Matthew Tuusberg <tuusberg@gmail.com>
2019-06-05 15:30:38 +0300

    update license

--- packages/react-storefront/src/utils/moovConfig.js
+++ packages/react-storefront/src/utils/moovConfig.js
@@ -1,3 +1,7 @@
+/**
+ * @license
+ * Copyright © 2017-2019 Moov Corporation.  All rights reserved.
+ */
 import isString from 'lodash/isString'
 import isEmpty from 'lodash/isEmpty'
 import forEach from 'lodash/forEach'
