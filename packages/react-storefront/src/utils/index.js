8e976b6234015b58ed6981847b9bfc9c4ee73c1e
Richard van der Dys <richard.vanderdys@gmail.com>
2019-07-25 17:30:23 +0300

    v5.13.1

--- packages/react-storefront/src/utils/index.js
+++ packages/react-storefront/src/utils/index.js
@@ -8,7 +8,6 @@
 export { default as browser } from './browser.js';
 export { default as createGenerateClassName } from './createGenerateClassName.js';
 export { default as dataProps } from './dataProps.js';
-;
 export { default as scriptInjector } from './scriptInjector.js';
 export { default as state } from './state.js';
 export { default as url } from './url.js';
