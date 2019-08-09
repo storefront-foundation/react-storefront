5078696f8fa341d2f0fa552f432537c0e78fd3f9
Mark Brocato <mark.brocato@moovweb.com>
2019-08-09 03:56:38 -0700

    Added --analyze

--- packages/react-storefront-moov-xdn/bin/commands/build.js
+++ packages/react-storefront-moov-xdn/bin/commands/build.js
@@ -2,14 +2,23 @@ module.exports = {
   command: 'build',
   desc: 'build your app for production',
   builder: yargs => {
-    return yargs.option('environment', {
-      alias: 'e',
-      describe:
-        'The name of the environment to build, corresponding to a key in the "environments" under "moovweb" in package.json.',
-      default: process.env.moov_env || 'production'
-    })
+    return yargs
+      .option('environment', {
+        alias: 'e',
+        describe:
+          'The name of the environment to build, corresponding to a key in the "environments" under "moovweb" in package.json.',
+        default: process.env.moov_env || 'production'
+      })
+      .option('analyze', {
+        alias: 'a',
+        descript: 'Perform a size analysis on the client bundle.'
+      })
   },
-  handler: ({ environment }) => {
+  handler: ({ environment, analyze }) => {
+    if (analyze) {
+      process.env.ANALYZE = 'true'
+    }
+
     require('../lib/build')(environment)
   }
 }
