43dfcc24683563521d1d3cc5ecdc8ba397aee85e
Mark Brocato <mark.brocato@moovweb.com>
2019-08-13 02:34:44 -0700

    Restored edge entry points with deprecation warning.

---
+++ packages/react-storefront-moov-xdn/src/edgeResponseTransform.js
@@ -0,0 +1,9 @@
+/**
+ * @license
+ * Copyright © 2017-2019 Moov Corporation.  All rights reserved.
+ */
+export default function edgeResponseTransform() {
+  console.warn(
+    'Edge transforms are deprecated and will be removed in a future release of React Storefront.  To hide this warning please upgrade to moovsdk@latest or set EdgeRequestChangeAllowed and EdgeResponseChangeAllowed to false in your moov_config-*.json files.'
+  )
+}
