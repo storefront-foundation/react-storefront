b6bf841cf8702682bdb3645bd3f4a1d7cf3e3a8d
Matthew Tuusberg <tuusberg@gmail.com>
2019-08-23 00:16:27 +0300

    get all the test results and store the output in a file

--- packages/react-storefront-selenium/smoke-test-reporter.js
+++ packages/react-storefront-selenium/smoke-test-reporter.js
@@ -1,30 +1,50 @@
+const fs = require('fs')
+
+const statusSuccess = 'success'
+const statusFailure = 'failure'
+const outputFile = process.env.JEST_OUTPUT_FILENAME || 'test-result.json'
+
+// Processes jest test results and saves them to a file.
+// See https://gimpneek.github.io/jest-reporter-debug/DocumentTestHooksReporter.html
 class SmokeTestReporter {
   constructor(globalConfig, options) {
     this._globalConfig = globalConfig
     this._options = options
   }
 
-  async onTestResult(test, testResult, aggregatedResult) {
+  async onRunComplete(test, runResults) {
+    const result = this.getResults(runResults)
+    await fs.writeFile(outputFile, JSON.stringify(result))
+  }
+
+  getResults(runResults) {
     const failedTests = {}
 
-    testResult.testResults
+    // Go over all failed test cases and grab their descriptions
+    const testSuiteResults = runResults.testResults
+    const testCaseResults = [].concat.apply([], testSuiteResults.map(test => test.testResults))
+    testCaseResults
       .filter(el => el.status === 'failed')
-      .forEach(test => {
-        if (test.ancestorTitles[0] in failedTests) {
-          return
+      .forEach(testCase => {
+        if (testCase.ancestorTitles[0] in failedTests) {
+          failedTests[testCase.ancestorTitles[0]].push(testCase.title)
+        } else {
+          failedTests[testCase.ancestorTitles[0]] = [testCase.title]
         }
-        failedTests[test.ancestorTitles[0]] = test.title
       })
 
-    const failedDescriptions = Object.keys(failedTests)
-
-    if (failedDescriptions.length) {
-      const output = failedDescriptions
-        .map(description => `${description} failed with: ${failedTests[description]}`)
+    const isSuccess = 0 === failedTests.length
+    let result = {}
+    if (isSuccess) {
+      result.status = statusSuccess
+    } else {
+      result.status = statusFailure
+      result.error = Object.keys(failedTests)
+        .map(name => `Test '${name}' failed with: ${failedTests[name].join(', ')}`)
         .join('\n')
-
-      console.log(output)
     }
+
+    return result
   }
 }
 
