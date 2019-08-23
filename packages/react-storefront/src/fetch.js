fdbe66ca691376a55ea103a2af423d2b1c04b7c4
Mark Brocato <mark.brocato@moovweb.com>
2019-08-23 16:12:36 -0700

    Added license

--- packages/react-storefront/src/fetch.js
+++ packages/react-storefront/src/fetch.js
@@ -1,2 +1,6 @@
+/**
+ * @license
+ * Copyright © 2017-2019 Moov Corporation.  All rights reserved.
+ */
 export { StaleResponseError, fetchLatest } from './fetchLatest'
 export { default as fetch } from 'react-storefront-moov-xdn'
