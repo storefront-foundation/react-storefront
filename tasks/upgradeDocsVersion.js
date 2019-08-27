32f35362f8b733e8f88e95ab2ffede535e1a9559
Mark Brocato <mark.brocato@moovweb.com>
2019-08-27 05:40:28 -0700

    Also upgrade react-storefront-moov-xdn in docs upon release.

--- tasks/upgradeDocsVersion.js
+++ tasks/upgradeDocsVersion.js
@@ -4,8 +4,9 @@ const fs = require('fs')
 const version = require('../lerna.json').version
 
 // Upgrade docs RSF dependency version to current
-const docsPackage = require('../docs/package.json');
-docsPackage.devDependencies['react-storefront'] = version;
+const docsPackage = require('../docs/package.json')
+docsPackage.devDependencies['react-storefront'] = version
+docsPackage.devDependencies['react-storefront-moov-xdn'] = version
 
 // Write it
 fs.writeFileSync(__dirname + '/../docs/package.json', JSON.stringify(docsPackage, null, 2))
