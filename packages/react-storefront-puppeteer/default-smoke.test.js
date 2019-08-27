a8019d741b5de5a49bf2a5a3e5cf5f5cea95b114
Matthew Tuusberg <tuusberg@gmail.com>
2019-08-27 12:56:26 +0300

    simplify tests

--- packages/react-storefront-puppeteer/default-smoke.test.js
+++ packages/react-storefront-puppeteer/default-smoke.test.js
@@ -8,7 +8,7 @@
  * The hostname of the app to be tested is specified in the required environment variable `RSF_HOST`.
  */
 
-const { createBrowser, createPage } = require('./index')
+const { createBrowser, createPage, clickElement } = require('./index')
 
 const startURL = process.env.RSF_URL
 const sleepBetweenPages = process.env.RSF_SLEEP_BETWEEN_PAGES || 2000
@@ -46,27 +46,23 @@ describe('smoke tests', () => {
   })
 
   it('Navigate to category', async () => {
-    await clickElement(page, '[data-th="topNavClicked"]')
+    await clickElement(page, '[data-th="nav"]')
   })
 
   it('Navigate to subcategory', async function() {
-    const subcategories = await page.$x('//a[contains(@href, "s/1")]')
-    await subcategories[0].click()
+    await clickElement(page, '[data-th="subcategory-link"]')
   })
 
   it('Navigate to product', async function() {
-    const products = await page.$x('//a[contains(@href, "p/1")]')
-    await products[0].click()
+    await clickElement(page, '[data-th="product-link"]')
   })
 
   it('Add product to cart ', async function() {
-    const addToCartElement = await page.$x('//span[contains(text(), "Add to Cart")]')
-    await addToCartElement[0].click()
+    await clickElement(page, '[data-th="add-to-cart"]')
   })
 
   it('Navigate to cart', async function() {
-    const cartBtn = await page.$x('//button[@aria-label="Cart"]')
-    await cartBtn[0].click()
+    await clickElement(page, '[data-th="cart-link"]')
   })
 
   it('Verify product in cart', async function() {
@@ -75,7 +71,6 @@ describe('smoke tests', () => {
   })
 
   it('Navigate to checkout', async function() {
-    const checkoutElement = await page.$x('//span[contains(text(), "Checkout")]')
-    await checkoutElement[0].click()
+    await clickElement(page, '[data-th="checkout-link"]')
   })
 })
