f77bee6c31a5022296a958cb976c390a1fcfea7d
Mark Brocato <mark.brocato@moovweb.com>
2019-09-13 15:24:51 +0300

    add copyright

--- packages/react-storefront/src/router/fromServer.js
+++ packages/react-storefront/src/router/fromServer.js
@@ -1,2 +1,6 @@
+/**
+ * @license
+ * Copyright © 2017-2019 Moov Corporation.  All rights reserved.
+ */
 import fromServer from './state'
 export default fromServer
--- packages/react-storefront/src/router/page.js
+++ packages/react-storefront/src/router/page.js
@@ -1,3 +1,7 @@
+/**
+ * @license
+ * Copyright © 2017-2019 Moov Corporation.  All rights reserved.
+ */
 import fromClient from './fromClient'
 
 /**
