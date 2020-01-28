eebfad5daafee269efb559e60f2de48393fa3f47
Leonid Tolli <leonid.tolli@gmail.com>
2020-01-28 15:35:26 +0200

    withCaching args change

--- src/utils/withCaching.js
+++ src/utils/withCaching.js
@@ -6,10 +6,10 @@
  * @param {Function} defaultHandler A handler to run unless `e.preventDefault()` was called.
  * @return {Function}
  */
-export default function withCaching(handler, options) {
+export default function withCaching(handler, { browser, edge }) {
   return (req, res) => {
-    if (options.browser.maxAgeSeconds) {
-      res.setHeader('x-rsf-cache-control', `max-age: ${options.browser.maxAgeSeconds}`)
+    if (browser.maxAgeSeconds) {
+      res.setHeader('x-rsf-cache-control', `max-age: ${browser.maxAgeSeconds}`)
     }
 
     return handler(req, res)
