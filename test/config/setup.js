8d73312837243d9426c9b07dccef5b10b78e58d2
kevhender <kevhender@gmail.com>
2020-02-14 17:14:48 -0500

    Fix babel transform for jest + rewire

--- test/config/setup.js
+++ test/config/setup.js
@@ -1,6 +1,6 @@
-import Enzyme from 'enzyme'
-import EnzymeAdapter from 'enzyme-adapter-react-16'
-import sleep from './sleep'
+const Enzyme = require('enzyme')
+const EnzymeAdapter = require('enzyme-adapter-react-16')
+const sleep = require('./sleep').default
 
 // Setup enzyme's react adapter
 Enzyme.configure({ adapter: new EnzymeAdapter() })
