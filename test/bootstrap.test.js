abb62cc3290ebfc632560440b33bc938e4824f78
kevhender <kevhender@gmail.com>
2020-02-14 12:07:33 -0500

    Add rewire to help with testing service worker bootstrap

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -1,6 +1,8 @@
 import makeServiceWorkerEnv from 'service-worker-mock'
 import makeFetchMock from 'service-worker-mock/fetch'
 
+let sw
+
 describe('bootstrap', () => {
   beforeEach(() => {
     const serviceWorkerEnv = makeServiceWorkerEnv()
@@ -15,7 +17,7 @@ describe('bootstrap', () => {
     }
     Object.assign(global, serviceWorkerEnv, makeFetchMock())
     jest.resetModules()
-    require('../service-worker/bootstrap')
+    sw = require('../service-worker/bootstrap')
   })
 
   it('should add data to the cache', async () => {
@@ -34,5 +36,62 @@ describe('bootstrap', () => {
     expect(cachedValue).toEqual(cacheData)
   })
 
-  it.todo('should abort prefetches when fetching more important resources')
+  describe('abortPrefetches', () => {
+    it('should abort existing prefetches', () => {
+      const abortPrefetches = sw.__get__('abortPrefetches')
+      const abortControllers = sw.__get__('abortControllers')
+      abortControllers.add(new AbortController())
+      expect(abortControllers.size).toEqual(1)
+      abortPrefetches()
+      expect(abortControllers.size).toEqual(0)
+    })
+
+    it('should add aborted prefetches to resume list', () => {
+      const abortPrefetches = sw.__get__('abortPrefetches')
+      const abortControllers = sw.__get__('abortControllers')
+      const toResume = sw.__get__('toResume')
+      abortControllers.add(new AbortController())
+      expect(toResume.size).toEqual(0)
+      abortPrefetches()
+      expect(toResume.size).toEqual(1)
+    })
+  })
+
+  describe('resumePrefetches', () => {
+    it('should resume aborted prefetches', () => {
+      const cachePath = jest.fn()
+      sw.__set__('cachePath', cachePath)
+      const toResume = sw.__get__('toResume')
+      const resumePrefetches = sw.__get__('resumePrefetches')
+      toResume.add([{ path: '', apiVersion: 'v1' }])
+      resumePrefetches()
+      expect(cachePath).toHaveBeenCalled()
+    })
+
+    it('should clear the resume list when done resuming', () => {
+      const toResume = sw.__get__('toResume')
+      const resumePrefetches = sw.__get__('resumePrefetches')
+      toResume.add([{ path: '', apiVersion: 'v1' }])
+      resumePrefetches()
+      expect(toResume.size).toEqual(0)
+    })
+  })
+
+  describe('fetch', () => {
+    it('should abort prefetches when fetching more important resources', () => {
+      const abortControllers = sw.__get__('abortControllers')
+      const toResume = sw.__get__('toResume')
+      abortControllers.add(new AbortController())
+      self.trigger('fetch')
+      expect(abortControllers.size).toEqual(0)
+      expect(toResume.size).toEqual(1)
+    })
+
+    it('should resume prefetches when non-prefetch fetch is done', async () => {
+      const toResume = sw.__get__('toResume')
+      toResume.add([{ path: '', apiVersion: 'v1' }])
+      await self.trigger('fetch')
+      expect(toResume.size).toEqual(0)
+    })
+  })
 })
