b0a70afe387d59e41b3fd08dd5d4629871d04dc0
Mark Brocato <mark.brocato@moovweb.com>
2020-02-25 15:50:25 +0200

    Rename x-rsf-cache-control to x-sw-cache-control.

--- test/utils/withCaching.test.js
+++ test/utils/withCaching.test.js
@@ -1,18 +1,18 @@
 import withCaching from 'react-storefront/utils/withCaching'
 
 describe('withCaching', () => {
-  it('should set header if maxAgeSeconds are present on the browser', () => {
+  it('should set header if maxAgeSeconds is present', () => {
     const res = { setHeader: jest.fn() }
 
-    withCaching(jest.fn(), { browser: { maxAgeSeconds: 10 } })({}, res)
+    withCaching(jest.fn(), 10)({}, res)
 
     expect(res.setHeader).toBeCalled()
   })
 
-  it('should not set header if maxAgeSeconds are not present on the browser', () => {
+  it('should not set header if maxAgeSeconds are not present', () => {
     const res = { setHeader: jest.fn() }
 
-    withCaching(jest.fn(), { browser: {} })({}, res)
+    withCaching(jest.fn())({}, res)
 
     expect(res.setHeader).not.toBeCalled()
   })
