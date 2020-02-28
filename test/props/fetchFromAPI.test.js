cf43590d6d0650e1e7f9598887295179589e6011
Mark Brocato <mark.brocato@moovweb.com>
2020-02-28 15:29:06 +0200

    Add support for calling a separate api host in fetchFromAPI based on an API_HOST environment variable.

--- test/props/fetchFromAPI.test.js
+++ test/props/fetchFromAPI.test.js
@@ -1,95 +1,124 @@
+import fetchFromAPI from 'react-storefront/props/fetchFromAPI'
+
 describe('fetchFromAPI', () => {
-  const headers = {
-    headers: { 'x-rsf-api-version': '1' },
-  }
+  let headers
 
   fetchMock.mockResponse(JSON.stringify({}))
 
-  let fetchFromAPI,
-    isBrowser = true
-
-  beforeEach(() => {
-    jest.isolateModules(() => {
-      jest.doMock('react-storefront/utils/isBrowser', () => () => isBrowser)
-      fetchFromAPI = require('react-storefront/props/fetchFromAPI').default
-    })
-  })
-
-  afterAll(() => {
-    jest.resetAllMocks()
-  })
-
   describe('in the browser', () => {
     beforeEach(() => {
-      isBrowser = true
+      headers = {
+        'x-rsf-api-version': '1',
+      }
     })
 
     it('should prepend api to the path', () => {
       fetchFromAPI({
         asPath: '/p/1',
       })
-      expect(fetchMock).toHaveBeenCalledWith('/api/p/1', headers)
+      expect(fetchMock).toHaveBeenCalledWith('/api/p/1', { headers })
     })
 
     it('should call /api when the path is /', () => {
       fetchFromAPI({
         asPath: '/',
       })
-      expect(fetchMock).toHaveBeenCalledWith('/api', headers)
+      expect(fetchMock).toHaveBeenCalledWith('/api', { headers })
     })
 
     it('should append query params directly to api if the root with query params is called', () => {
       fetchFromAPI({
         asPath: '/?test=1',
       })
-      expect(fetchMock).toHaveBeenCalledWith('/api?test=1', headers)
+      expect(fetchMock).toHaveBeenCalledWith('/api?test=1', { headers })
     })
   })
 
   describe('on the server', () => {
     beforeEach(() => {
-      isBrowser = false
+      headers = {
+        'x-rsf-api-version': '1',
+        host: 'www.domain.com',
+      }
     })
 
     it('should include the protocol, domain, and ?_includeAppData=1', () => {
       fetchFromAPI({
         asPath: '/p/1',
+        pathname: '/p/[productId]',
         req: {
           headers: {
             host: 'www.domain.com',
           },
         },
       })
-      expect(fetchMock).toHaveBeenCalledWith(
-        'https://www.domain.com/api/p/1?_includeAppData=1',
-        headers,
-      )
+      expect(fetchMock).toHaveBeenCalledWith('https://www.domain.com/api/p/1?_includeAppData=1', {
+        headers: {
+          ...headers,
+          'x-next-page': '/api/p/[productId]',
+        },
+      })
+    })
+
+    it('should use API_HOST when provided', () => {
+      process.env.API_HOST = 'localhost:3001'
+
+      fetchFromAPI({
+        asPath: '/p/1',
+        pathname: '/p/[productId]',
+        req: {
+          headers: {
+            host: 'www.domain.com',
+          },
+        },
+      })
+
+      expect(fetchMock).toHaveBeenCalledWith('http://localhost:3001/api/p/1?_includeAppData=1', {
+        headers: {
+          ...headers,
+          'x-next-page': '/api/p/[productId]',
+        },
+      })
+
+      delete process.env.API_HOST
     })
 
     it('should use http:// for localhost', () => {
       fetchFromAPI({
         asPath: '/p/1',
+        pathname: '/p/[productId]',
         req: {
           headers: {
+            ...headers,
             host: 'localhost',
           },
         },
       })
-      expect(fetchMock).toHaveBeenCalledWith('http://localhost/api/p/1?_includeAppData=1', headers)
+      expect(fetchMock).toHaveBeenCalledWith('http://localhost/api/p/1?_includeAppData=1', {
+        headers: {
+          ...headers,
+          host: 'localhost',
+          'x-next-page': '/api/p/[productId]',
+        },
+      })
     })
 
     it('should append _includeAppData to the existing query string', () => {
       fetchFromAPI({
         asPath: '/foo?x=1',
+        pathname: '/foo',
         req: {
-          headers: {
-            host: 'localhost',
-          },
+          headers,
         },
       })
       expect(fetchMock).toHaveBeenCalledWith(
-        'http://localhost/api/foo?x=1&_includeAppData=1',
-        headers,
+        'https://www.domain.com/api/foo?x=1&_includeAppData=1',
+        {
+          headers: {
+            ...headers,
+            'x-next-page': '/api/foo',
+          },
+        },
       )
     })
   })
