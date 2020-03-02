5199feb7119d968fd853b4bbe6355cfd28763ad7
Leonid Tolli <leonid.tolli@gmail.com>
2020-03-02 17:40:55 +0200

    Merge fix

--- src/search/SearchField.js
+++ src/search/SearchField.js
@@ -1,5 +1,5 @@
 import React, { useRef, forwardRef } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, fade } from '@material-ui/core/styles'
 import PropTypes from 'prop-types'
 import { IconButton } from '@material-ui/core'
 import ClearIcon from '@material-ui/icons/Clear'
