fb5c3df6b4b75656bf68a1f16e25590e00e5decb
Tony Lepmets <tony.lepmets@moovweb.com>
2020-03-04 19:09:27 +0200

    Extract babel & jest config from package.json
    Jest doesn't read babel configuration correctly when part of package.json

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  clearMocks: true,
+  testEnvironment: './test/config/jsdom',
+  setupFiles: ['./test/config/setup.js'],
+  setupFilesAfterEnv: ['./test/config/mocks.js', './node_modules/jest-enzyme/lib/index.js'],
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**/*.{js,jsx}', './service-worker/*.{js,jsx}', '!**/index.js'],
+  moduleNameMapper: {
+    'react-storefront/(.*)': '<rootDir>/src/$1',
+  },
+  transform: {
+    '.*': './test/config/babelTransformer.js',
+  },
+}
