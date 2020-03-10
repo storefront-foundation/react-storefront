e516af7f993a5797df7c7f7308547cc853b75548
Mark Brocato <mark.brocato@moovweb.com>
2020-03-11 00:02:50 +0200

    Fixes and issue with building the service worker.

--- plugins/withReactStorefront.js
+++ plugins/withReactStorefront.js
@@ -3,85 +3,67 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const API_VERSION = new Date().getTime()
 const withServiceWorker = require('./withServiceWorker')
 const ClearRequireCachePlugin = require('webpack-clear-require-cache-plugin')
-const chalk = require('chalk')
 
 module.exports = (nextConfig = {}) => {
   const usePreact = process.env.preact === 'true'
 
-  return phase => {
-    const bootstrapOptions = {
-      prefetchRampUpTime: -5000,
-      allowPrefetchThrottling: false,
-      serveSSRFromCache: false,
-    }
+  return withServiceWorker({
+    ...nextConfig,
+    target: 'serverless',
+    webpack(config, options) {
+      config.resolve.symlinks = false
 
-    // if debugging service workers, options can be changed in Dev phase:
-    if (phase === PHASE_DEVELOPMENT_SERVER) {
-      bootstrapOptions.allowPrefetchThrottling = true
-    }
+      if (usePreact) {
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          react: 'preact/compat',
+          react$: 'preact/compat',
+          'react-dom/test-utils': 'preact/test-utils',
+          'react-dom': 'preact/compat',
+          'react-dom$': 'preact/compat',
+        }
+      }
 
-    const { bootstrapPath, makeCopyOptions } = copyBootstrap(bootstrapOptions)
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          'process.env.RSF_API_VERSION': JSON.stringify(API_VERSION),
+        }),
+      )
 
-    return withServiceWorker(
-      {
-        ...nextConfig,
-        target: 'serverless',
-        webpack(config, options) {
-          config.resolve.symlinks = false
+      if (process.env.NODE_ENV === 'development') {
+        // This makes it easier to develop apps against a local clone of react-storefront linked with yalc. Here
+        // we ensure that the server build recompiles when any linked dependency changes.
+        config.plugins.push(
+          new ClearRequireCachePlugin([
+            /\.next\/server\/ssr-module-cache.js/,
+            /react-storefront-analytics/,
+            /react-storefront-amp/,
+            /react-storefront\//,
+          ]),
+        )
+      }
 
-          if (usePreact) {
-            config.resolve.alias = {
-              ...config.resolve.alias,
-              react: 'preact/compat',
-              react$: 'preact/compat',
-              'react-dom/test-utils': 'preact/test-utils',
-              'react-dom': 'preact/compat',
-              'react-dom$': 'preact/compat',
-            }
-          }
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      })
 
-          config.plugins.push(
-            new webpack.DefinePlugin({
-              'process.env.RSF_API_VERSION': JSON.stringify(API_VERSION),
-            }),
-          )
+      if (!options.isServer && process.env.analyze === 'true') {
+        config.plugins.push(new BundleAnalyzerPlugin())
+      }
 
-          if (process.env.NODE_ENV === 'development') {
-            // This makes it easier to develop apps against a local clone of react-storefront linked with yalc. Here
-            // we ensure that the server build recompiles when any linked dependency changes.
-            config.plugins.push(
-              new ClearRequireCachePlugin([
-                /\.next\/server\/ssr-module-cache.js/,
-                /react-storefront-analytics/,
-                /react-storefront-amp/,
-                /react-storefront\//,
-              ]),
-            )
-          }
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
 
-          config.module.rules.push({
-            test: /\.svg$/,
-            use: ['@svgr/webpack'],
-          })
-
-          if (!options.isServer && process.env.analyze === 'true') {
-            config.plugins.push(new BundleAnalyzerPlugin())
-          }
-
-          if (typeof nextConfig.webpack === 'function') {
-            return nextConfig.webpack(config, options)
-          }
-
-          return config
-        },
-        webpackDevMiddleware(config) {
-          config.watchOptions = {
-            // required to recompile client build when there are changes in node_modules
-            ignored: [],
-          }
-          return config
-        },
-      },
-    )
-  }
+      return config
+    },
+    webpackDevMiddleware(config) {
+      config.watchOptions = {
+        // required to recompile client build when there are changes in node_modules
+        ignored: [],
+      }
+      return config
+    },
+  })
 }
