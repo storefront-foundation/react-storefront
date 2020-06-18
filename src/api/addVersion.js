a519cec4a3568b4541b9480292e4b71932165b6d
Mark Brocato <mark.brocato@moovweb.com>
2020-06-18 11:59:09 +0300

    Reuse parsed URL

--- src/api/addVersion.js
+++ src/api/addVersion.js
@@ -8,24 +8,28 @@ export const VERSION_PARAM = '__v__'
 /**
  * Adds the next build id as the __v__ query parameter to the given url if
  * the hostname is the same as the URL from which the app was served.
- * @param {String} url Any URL
+ * @param {URL|string} url Any URL
  * @return {URL}
  */
 export default function addVersion(url) {
-  /* istanbul ignore next */
-  if (typeof window === 'undefined') return new URL(url, 'http://throwaway.api')
+  const appOrigin = typeof window !== 'undefined' ? window.location.href : 'http://throwaway.api'
 
-  const parsed = new URL(url, window.location.href)
+  if (typeof url === 'string') {
+    url = new URL(url, appOrigin)
+  }
+
+  /* istanbul ignore next */
+  if (typeof window === 'undefined') return url
 
   // we should only add ?__v__ for same-origin requests as this may break
   // requests to 3rd parties that have strict parameter validation such as firebase
   if (
-    parsed.hostname === location.hostname &&
-    !parsed.searchParams.has(VERSION_PARAM) &&
+    url.hostname === location.hostname &&
+    !url.searchParams.has(VERSION_PARAM) &&
     typeof __NEXT_DATA__ !== 'undefined'
   ) {
-    parsed.searchParams.append(VERSION_PARAM, __NEXT_DATA__.buildId)
+    url.searchParams.append(VERSION_PARAM, __NEXT_DATA__.buildId)
   }
 
-  return parsed
+  return url
 }
--- src/fetch.js
+++ src/fetch.js
@@ -1,4 +1,20 @@
 import addVersion from './api/addVersion'
+import isSameOrigin from './utils/isSameOrigin'
+
+/**
+ * Returns the parsed URL for the specified request
+ * @param {Request|String} request A request instance or a URL string
+ * @return {URL}
+ */
+function getURL(request) {
+  let url = request.url
+
+  if (typeof request === 'string') {
+    url = request
+  }
+
+  return new URL(url, window.location.href)
+}
 
 // Here we patch fetch and XMLHttpRequest to always add version parameter to api calls so that cached results
 // from previous versions of the app aren't served to new versions.
@@ -6,12 +22,18 @@ import addVersion from './api/addVersion'
 if (typeof window !== 'undefined') {
   const originalFetch = window.fetch
 
-  window.fetch = function rsfVersionedFetch(url, init) {
-    if (url.url) {
-      // the first param can be a request object
-      url = new Request(addVersion(url.url).toString(), url)
+  window.fetch = function rsfVersionedFetch(url, init, ...others) {
+    const parsed = getURL(url)
+
+    if (!isSameOrigin(parsed)) {
+      return originalFetch(url, init)
+    }
+
+    if (typeof url === 'string') {
+      url = addVersion(parsed).toString()
     } else {
-      url = addVersion(url).toString()
+      // the first param can be a request object
+      url = new Request(addVersion(parsed).toString(), url)
     }
 
     return originalFetch(url, init)
@@ -23,7 +45,13 @@ if (typeof XMLHttpRequest !== 'undefined') {
   const originalOpen = XMLHttpRequest.prototype.open
 
   XMLHttpRequest.prototype.open = function rsfVersionedOpen(method, url, ...others) {
-    return originalOpen.call(this, method, addVersion(url).toString(), ...others)
+    const parsed = getURL(url)
+
+    if (isSameOrigin(parsed)) {
+      return originalOpen.call(this, method, addVersion(parsed).toString(), ...others)
+    } else {
+      return originalOpen.call(this, method, url, ...others)
+    }
   }
 }
 
