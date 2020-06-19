6fcff2ed4994576e6b5d1bee2f11d49fe5ebfaf9
Mark Brocato <mark.brocato@moovweb.com>
2020-06-19 11:44:10 +0300

    remove test corresponding to last change.

--- test/api/addVersion.test.js
+++ test/api/addVersion.test.js
@@ -21,10 +21,6 @@ describe('addVersion', () => {
     )
   })
 
-  it('should not add the version query param unless the request is same-origin', () => {
-    expect(addVersion('https://some-domain.com/foo').toString()).toBe('https://some-domain.com/foo')
-  })
-
   it('should not duplicate the version query param', () => {
     expect(addVersion('/foo?__v__=1').toString()).toBe('http://localhost/foo?__v__=1')
   })
