3cd977b1e4c535db233b98d11fec05e438ce95fc
Richard van der Dys <richard.vanderdys@gmail.com>
2020-08-03 12:38:49 +0300

    SearchProvider updates
    - Added minQueryLength and debounceTime props for better configuration
    - Fixed debounce fetching

--- test/search/SearchProvider.test.js
+++ test/search/SearchProvider.test.js
@@ -60,12 +60,40 @@ describe('SearchProvider', () => {
     fetchMock.mockResponseOnce(JSON.stringify({ groups: 'test2' }))
 
     await act(async () => {
-      wrapper.setProps({ query: 'a' })
+      wrapper.setProps({ query: 'abc' })
       await sleep(300) // to trigger debounce
       await wrapper.update()
     })
 
     expect(context.state.groups).toBe('test2') // check that suggestions are fetched only after active is set to true
+  })
+
+  it('should not fetch suggestions if query length is less than minimum', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify({ groups: 'test' }))
+
+    wrapper = mount(
+      <SearchProvider query="" active>
+        <ContextGetter />
+      </SearchProvider>,
+    )
+
+    await act(async () => {
+      await sleep(300) // to trigger debounce
+      await wrapper.update()
+    })
+
+    expect(context.state.groups).toBe('test') // check that suggestions aren't fetched on mount
+
+    fetchMock.mockResponseOnce(JSON.stringify({ groups: 'test2' }))
+
+    await act(async () => {
+      // Using a query less than default min length
+      wrapper.setProps({ query: 'a' })
+      await sleep(300) // to trigger debounce
+      await wrapper.update()
+    })
+
+    expect(context.state.groups).toBe('test')
   })
 
   it('should catch fetch errors and set loading to false', async () => {
