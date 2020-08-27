4ce6c1f01941432ac9103d29e9ac2704828388cf
Richard van der Dys <richard.vanderdys@gmail.com>
2020-08-27 17:52:08 +0300

    Fixed API response issue
    Cleaned up how we pull data product data

--- src/mock-connector/addToCart.js
+++ src/mock-connector/addToCart.js
@@ -1,10 +1,10 @@
-export default async function addToCart(product, quantity, req, res) {
-  const { id, color, size } = product
+export default async function addToCart({ color, size, product }, req, res) {
+  const { id, quantity } = product
 
   console.log('product id: ', id)
   console.log('color id: ', color || 'Not provided')
   console.log('size id: ', size || 'Not provided')
-  console.log('quantity ', quantity)
+  console.log('quantity ', quantity || 1)
 
-  return {}
+  return { added: true, id }
 }
