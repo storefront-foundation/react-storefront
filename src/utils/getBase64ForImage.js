f29bcb3e4f643977bf0b8bb1134d78a5d6722c26
Richard van der Dys <richard.vanderdys@gmail.com>
2020-12-29 18:29:59 +0200

    Changed mock image service (#161)

--- src/utils/getBase64ForImage.js
+++ src/utils/getBase64ForImage.js
@@ -1,7 +1,7 @@
 import fetch from '../fetch'
 
-export default async function getBase64ForImage(src) {
-  const res = await fetch(src)
+export default async function getBase64ForImage(...args) {
+  const res = await fetch(...args)
   const contentType = res.headers.get('content-type')
   const buffer = await res.buffer()
   return `data:${contentType};base64,${buffer.toString('base64')}`
