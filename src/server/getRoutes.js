698425cb30eedcf9aa6f234c812ec9f4b131f181
Divyakumar <divyakumarjain@gmail.com>
2021-07-22 10:29:08 +0530

    Upgrade to next11

--- src/server/getRoutes.js
+++ src/server/getRoutes.js
@@ -1,4 +1,4 @@
-import { getRouteRegex } from 'next/dist/next-server/lib/router/utils/route-regex'
+import { getRouteRegex } from 'next/dist/shared/lib/router/utils/route-regex'
 
 export default function routes(pagesManifest) {
   const routes = {}
